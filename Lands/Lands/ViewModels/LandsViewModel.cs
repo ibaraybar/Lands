a9c9ac7296c49822f929ddd3421c063391831f74
ibaraybar <ibaraybard@gmail.com>
2018-06-03 01:59:25 -0500

    Load countries and show in ListView

--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,10 +1,12 @@
 ﻿namespace Lands.ViewModels
 {
+   using GalaSoft.MvvmLight.Command;
    using Lands.Models;
    using Lands.Services;
    using System;
    using System.Collections.Generic;
    using System.Collections.ObjectModel;
+   using System.Windows.Input;
    using Xamarin.Forms;
 
    public class LandsViewModel : BaseViewModel
@@ -15,6 +17,7 @@
 
       #region Atributes
       private ObservableCollection<Land> lands;
+      private bool isRefreshing;
       #endregion
 
       #region Properties
@@ -22,6 +25,12 @@
       {
          get { return this.lands; }
          set { SetValue(ref this.lands, value); }
+      }
+
+      public bool IsRefreshing
+      {
+         get { return this.isRefreshing; }
+         set { SetValue(ref this.isRefreshing, value); }
       }
       #endregion
 
@@ -36,10 +45,12 @@
       #region Methods
       private async void LoadLands()
       {
+         this.IsRefreshing = true;
          var connection = await this.apiService.CheckConnection();
 
          if (!connection.IsSuccess)
          {
+            this.IsRefreshing = false;
             await Application.Current.MainPage.DisplayAlert(
                "Error",
                connection.Message,
@@ -55,6 +66,7 @@
 
          if (!response.IsSuccess)
          {
+            this.IsRefreshing = false;
             await Application.Current.MainPage.DisplayAlert(
                "Error", 
                response.Message, 
@@ -65,6 +77,17 @@
 
          var list = (List<Land>)response.Result;
          this.Lands = new ObservableCollection<Land>(list);
+         this.IsRefreshing = false;
+      }
+      #endregion
+
+      #region Commands
+      public ICommand RefreshCommand
+      {
+         get
+         {
+            return new RelayCommand(LoadLands);
+         }
       }
       #endregion
    }
