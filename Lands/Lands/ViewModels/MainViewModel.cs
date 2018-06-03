1d6d05388591cfa038174b301cf38e27ca44e360
ibaraybar <ibaraybard@gmail.com>
2018-06-02 21:48:33 -0500

    Changes in LoginViewModel with base view model

--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lands.ViewModels
+﻿namespace Lands.ViewModels
 {
     public class MainViewModel
     {
-    }
+      #region ViewModels
+      public LoginViewModel Login { get; set; }
+      #endregion
+
+      #region Constructors
+      public MainViewModel()
+      {
+         this.Login = new LoginViewModel();
+      }
+      #endregion
+   }
 }
