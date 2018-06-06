3d55b7a9e2731c1b46ddc68358f803eeddfdedfa
ibaraybar <ibaraybard@gmail.com>
2018-06-05 20:20:52 -0500

    Show Land detail

--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -22,6 +22,7 @@ namespace Lands.ViewModels
       #region ViewModels
       public LoginViewModel Login { get; set; }
       public LandsViewModel Lands { get; set; }
+      public LandViewModel Land { get; set; }
       #endregion
 
       #region Constructors
