4d88761498db98e85d3e4fc71216882160a30fa2
ibaraybar <ibaraybard@gmail.com>
2018-06-07 19:01:57 -0500

    Automatic Migrations

--- Lands.Backend/Global.asax.cs
+++ Lands.Backend/Global.asax.cs
@@ -1,26 +1,28 @@
 ﻿namespace Lands.Backend
 {
    using Lands.Backend.Helpers;
+   using System.Data.Entity;
    using System.Web.Mvc;
    using System.Web.Optimization;
    using System.Web.Routing;
 
    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            this.CheckRolesAndSuperUser();
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
+   {
+      protected void Application_Start()
+      {
+         Database.SetInitializer(new MigrateDatabaseToLatestVersion<Models.LocalDataContext, Migrations.Configuration>());
+         this.CheckRolesAndSuperUser();
+         AreaRegistration.RegisterAllAreas();
+         FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+         RouteConfig.RegisterRoutes(RouteTable.Routes);
+         BundleConfig.RegisterBundles(BundleTable.Bundles);
+      }
 
-         private void CheckRolesAndSuperUser()
-         {
-            UsersHelper.CheckRole("Admin");
-            UsersHelper.CheckRole("User");
-            UsersHelper.CheckSuperUser();
-         }
+      private void CheckRolesAndSuperUser()
+      {
+         UsersHelper.CheckRole("Admin");
+         UsersHelper.CheckRole("User");
+         UsersHelper.CheckSuperUser();
+      }
    }
 }
